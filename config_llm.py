cdc4d2a2b39a6e0fbc8e54d149315509e3f02b33
Showmick Kar <showmick12@gmail.com>
2024-01-28 20:48:29 +0600

    Update config_llm.py

--- config_llm.py
+++ config_llm.py
@@ -1,8 +1,72 @@
-from transformers import pipeline
+import torch
+import gradio as gr
 
-pipe = pipeline("text-generation", model="mistralai/Mistral-7B-v0.1")
+from textwrap import fill
+from IPython.display import Markdown, display
 
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,                                                                                                       
+    SystemMessagePromptTemplate,
+    )
 
-tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-v0.1")
-model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-v0.1")
+from langchain import PromptTemplate
+from langchain import HuggingFacePipeline
+
+from langchain.vectorstores import Chroma
+from langchain.schema import AIMessage, HumanMessage
+from langchain.memory import ConversationBufferMemory
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.document_loaders import UnstructuredMarkdownLoader, UnstructuredURLLoader
+from langchain.chains import LLMChain, SimpleSequentialChain, RetrievalQA, ConversationalRetrievalChain
+
+from transformers import BitsAndBytesConfig, AutoModelForCausalLM, AutoTokenizer, GenerationConfig, pipeline
+
+import warnings
+warnings.filterwarnings('ignore')
+
+
+MODEL_NAME = "mistralai/Mistral-7B-Instruct-v0.1"
+
+quantization_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_compute_dtype=torch.float16,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_use_double_quant=True,
+)
+
+tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME, use_fast=True)
+tokenizer.pad_token = tokenizer.eos_token
+
+model = AutoModelForCausalLM.from_pretrained(
+    MODEL_NAME, torch_dtype=torch.float16,
+    trust_remote_code=True,
+    device_map="auto",
+    quantization_config=quantization_config
+)
+
+generation_config = GenerationConfig.from_pretrained(MODEL_NAME)
+generation_config.max_new_tokens = 1024
+generation_config.temperature = 0.0001
+generation_config.top_p = 0.95
+generation_config.do_sample = True
+generation_config.repetition_penalty = 1.15
+
+pipeline = pipeline(
+    "text-generation",
+    model=model,
+    tokenizer=tokenizer,
+    return_full_text=True,
+    generation_config=generation_config,
+)
+
+llm = HuggingFacePipeline(
+    pipeline=pipeline,
+    )
+
+embeddings = HuggingFaceEmbeddings(
+    model_name="thenlper/gte-large",
+    model_kwargs={"device": "cuda"},
+    encode_kwargs={"normalize_embeddings": True},
+)
