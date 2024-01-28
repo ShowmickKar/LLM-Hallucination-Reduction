9cd1ac785a3a8a179be1b130346bbb475f9b77b8
Showmick Kar <showmick12@gmail.com>
2024-01-28 20:49:10 +0600

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -3,10 +3,4 @@ import json
 path = "wikidata_questions.json"
 
 with open(path, 'r', encoding="utf8") as json_file:
-    data = json.load(json_file)
-
-print(len(data))
-
-for key in data:
-    print(len(data[key]))
-
+    wiki_data = json.load(json_file)
