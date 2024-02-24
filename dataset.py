6ea11ab9414910880df0afbe12ca98feb02ec570
Showmick Kar <showmick12@gmail.com>
2024-02-24 16:52:56 +0600

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,6 +1,14 @@
 import json
 
-path = "wikidata_questions.json"
 
-with open(path, 'r', encoding="utf8") as json_file:
+
+with open("wikidata_questions.json", 'r', encoding="utf8") as json_file:
     wiki_data = json.load(json_file)
+
+
+with open("wiki_category_dataset.json", 'r', encoding="utf8") as json_file:
+    wiki_category_dataset = json.load(json_file)
+
+
+with open("multispanqa_dataset.json", 'r', encoding="utf8") as json_file:
+    multispanqa_dataset = json.load(json_file)
