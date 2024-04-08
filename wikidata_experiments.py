92c2e907bd7ba04b821b0b2ebe0dd7dd2c7caa1b
Showmick Kar <showmick12@gmail.com>
2024-04-09 00:26:53 +0600

    Update wikidata_experiments.py

--- wikidata_experiments.py
+++ wikidata_experiments.py
@@ -1,7 +1,8 @@
 from itertools import islice
-from dataset import wiki_data
-from cov_wikidata import *
+from dataset import wiki_category_dataset
+from cov_wiki_category import *
 
+import json
 import time
 
 start = time.time()
@@ -22,35 +23,71 @@ fp_final = 0
 # small_list = dict(islice(wiki_data.items(), 3))
 # print(small_list)
 
+inspect_list = []
+
+
+
 p1 = []
 p2 = []
 
 step = 1
-for question in wiki_data:
-    intermediate_responses = wikidata_chain(question)
+for data in wiki_category_dataset:
+    intermediate_responses = wiki_category_chain(data["question"])
     baseline_response = intermediate_responses["baseline_response"]
     final_response = intermediate_responses["final_response"]
-    print(question)
+    print(data["question"])
     # print(baseline_response)
     # print(final_response)
-    tp, fp = evaluate_wikidata(baseline_response, wiki_data[question])
+    # tp, fp = evaluate_wikidata(baseline_response, data["answer"])
+    # tp_baseline += tp
+    # fp_baseline += fp
+    # tp, fp = evaluate_wikidata(final_response, data["answer"])
+    # tp_final += tp
+    # fp_final += fp
+
+    # precision_baseline = ((tp_baseline)/(tp_baseline+fp_baseline)) * 100
+    # precision_final = ((tp_final)/(tp_final+fp_final)) * 100
+
+    tp, fp = evaluate_wikidata(baseline_response, data["answer"])
+    precision_baseline = ((tp)/(tp+fp)) * 100
     tp_baseline += tp
     fp_baseline += fp
-    tp, fp = evaluate_wikidata(final_response, wiki_data[question])
+    tp, fp = evaluate_wikidata(final_response, data["answer"])
+    precision_final = ((tp)/(tp+fp)) * 100
     tp_final += tp
     fp_final += fp
 
-    precision_baseline = ((tp_baseline)/(tp_baseline+fp_baseline)) * 100
-    precision_final = ((tp_final)/(tp_final+fp_final)) * 100
+    precision_baseline_microaveraged = ((tp_baseline)/(tp_baseline+fp_baseline)) * 100
+    precision_final_microaveraged = ((tp_final)/(tp_final+fp_final)) * 100
 
-    p1.append(precision_baseline)
-    p2.append(precision_final)
+    # p1.append(precision_baseline)
+    # p2.append(precision_final)
 
     print(f"============= STEP {step} =============\nPrecision baseline on wiki data: {precision_baseline} and Precision Final on wiki data: {precision_final}")
+    item  = {'Step': step,
+             'Question': data["question"],
+             'Answer': data["answer"],
+             'Baseline_Response_Unparsed': intermediate_responses['baseline_unparsed'],
+             'Baseline_Response_Parsed': intermediate_responses['baseline_response'],
+             'Final_Response_Unparsed': intermediate_responses['final_response_unparsed'],
+             'Final_Response_Parsed': intermediate_responses['final_response'],
+             'Precision_Baseline_Response': precision_baseline,
+             'Precision_Final_Response': precision_final}
+    inspect_list.append(item)
     step += 1
 
-print(p1)
-print(p2)
+print(f"Microaveraged Precision Baseline: {precision_baseline_microaveraged}")
+print(f"Microaveraged Precision Final: {precision_final_microaveraged}")
+
 
 end = time.time()
-print(end - start)
+total_time = end - start
+
+print(total_time)
+
+inspect_list.append({"Microaveraged Precision Baseline": precision_baseline_microaveraged,
+                     "Microaveraged Precision Final": precision_final_microaveraged,
+                     "Time": total_time})
+
+with open("wiki_category_inspect.json", 'w') as json_file:
+    json.dump(inspect_list, json_file, indent=4, sort_keys=False)
